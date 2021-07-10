036dc5788a9ccff5f03f988b37d261309ef73f8d
mac <mac@localhost>
2021-07-10 14:07:00 +0100

    first commit

---
+++ cinema/src/main/java/org/sid/cinema/entities/Film.java
@@ -0,0 +1,33 @@
+package org.sid.cinema.entities;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import lombok.Data;
+@Entity
+@Data
+public class Film {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+   private String titre;
+   private String description;
+   private String realisateur;
+   private Date dateSortie;
+   private double duree;
+   private String photo;
+   @OneToMany(mappedBy="film")
+   @JsonProperty(access = Access.WRITE_ONLY)
+   private Collection<Projection> projections;
+   @ManyToOne
+   private Categorie categorie;
+}
